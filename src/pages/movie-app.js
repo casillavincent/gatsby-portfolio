2af02cf2312913f096d93379fabfc2c81226402f
Vincent <vincent@bstro.com>
2021-09-25 17:46:50 -0700

    Update Sep 25/21

--- src/pages/movie-app.js
+++ src/pages/movie-app.js
@@ -7,22 +7,25 @@ import ProjectTitle from "../components/ProjectComponents/ProjectTitle";
 import { projectTitles, projectOverview } from "../globals/info";
 
 const MovieApp = () => {
-    return (
-        <React.Fragment>
-            <Header theme="light" />
-            <Layout>
-                <ProjectTitle title={projectTitles[0].title} headline={projectTitles[0].headline} />
-                <ProjectOverview
-                    role={projectOverview[0].role}
-                    team={projectOverview[0].team}
-                    duration={projectOverview[0].duration}
-                    developmentStack={projectOverview[0].developmentStack}
-                    overview={projectOverview[0].overview}
-                />
-            </Layout>
-            <Footer />
-        </React.Fragment>
-    );
+	return (
+		<React.Fragment>
+			<Header theme="light" />
+			<Layout>
+				<ProjectTitle
+					title={projectTitles[0].title}
+					headline={projectTitles[0].headline}
+				/>
+				<ProjectOverview
+					role={projectOverview[0].role}
+					team={projectOverview[0].team}
+					duration={projectOverview[0].duration}
+					developmentStack={projectOverview[0].developmentStack}
+					overview={projectOverview[0].overview}
+				/>
+			</Layout>
+			<Footer />
+		</React.Fragment>
+	);
 };
 
 export default MovieApp;
